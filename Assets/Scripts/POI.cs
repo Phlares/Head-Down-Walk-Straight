90034d327ddc7a75069fb242bfcd3087935a4005
Phlares <ryon.everett@gmail.com>
2016-10-05 19:44:05 -0400

    Major update, added path finding to POI
    added Pathfinding to POI, have to make it only chase player when trigger zone is enabled

---
+++ Assets/Scripts/POI.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class POI : MonoBehaviour {
+
+	//Credits:
+	//Thank you http://answers.unity3d.com/questions/949222/is-raycast-efficient-in-update.html for helping me understand rays
+
+	//Declaring a hit container
+	public RaycastHit myHit;
+
+	//Declaring a Ray
+	Ray myRay;
+
+	//Set the game object/transform you want to shoot the ray from
+	//public Transform rayCastStart;
+
+	//Setting the Raycast Direction
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		myRay = new Ray(transform.position, transform.forward);
+		//A start position for the ray
+		Vector3 startMarker = transform.position;
+		CastRay();
+
+
+		//Do the RayCast each frame
+		//CastRay ();
+
+		//Drawing the ray with color and making it long (*1000)
+		Debug.DrawRay(startMarker, myRay.direction * 1000, Color.magenta);
+
+	}
+
+	public void CastRay(){
+		
+		//Actually casting the ray ==> Outputs collider to myHit ("out" keyword)
+		Physics.Raycast (myRay, out myHit, 15f );
+
+		//prints collider hit, filtering null errors
+		if (myHit.collider != null) {
+			Debug.Log (myHit.collider.gameObject);
+		} else {
+			
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+	public float currentMovementSpeed;
+	public bool canMove;
+	public float xAxis = 0;
+	public float zAxis = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Script borrowed from previous game jam "BadTrip" Matt Ferriera helped write it.
+	void FixedUpdate ()
+	{
+		xAxis = Input.GetAxisRaw("Horizontal"); //Getting values for X Axis
+		zAxis = Input.GetAxisRaw("Vertical");   //Getting values for Z Axis
+
+		if (xAxis != 0 || zAxis != 0)   //If there is movement, call method from PlayerController
+		{
+			Movement(xAxis, zAxis);
+		}
+
+
+	}
+		public void Movement(float xAxis, float zAxis)
+			{
+				if (xAxis != 0 && canMove == true)
+				{
+					if (xAxis > 0)  //Move right, else move left
+					{
+						gameObject.transform.Translate(new Vector3(currentMovementSpeed, 0, 0) * Time.deltaTime);
+					}
+					else
+					{
+						gameObject.transform.Translate(new Vector3(-currentMovementSpeed, 0, 0) * Time.deltaTime);
+					}
+				}
+
+				if (zAxis != 0 && canMove == true)
+				{
+					if (zAxis > 0) // Move forward else move backwards
+					{
+						gameObject.transform.Translate(new Vector3(0, 0, currentMovementSpeed) * Time.deltaTime);
+					}
+					else
+					{
+						gameObject.transform.Translate(new Vector3(0, 0, -currentMovementSpeed) * Time.deltaTime);
+					}
+				}
+			}
+}
